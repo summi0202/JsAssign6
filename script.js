d2ecbbf84c2412ae08f6e4516310135e8a52fe21
summi0202 <141727752+summi0202@users.noreply.github.com>
2024-01-07 14:57:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+//Q1 Find Grades.
+
+// let marks = Number(prompt("Enter your marks"));
+
+// if (marks <= 10) {
+//     console.log("Your grade is E");
+// } else if (marks >= 11 && marks <= 20) {
+//     console.log("Your Grade is D");
+// } else if (marks >= 21 && marks <= 30) {
+//     console.log("Your Grade is C ");
+// } else if (marks >= 31 && marks <= 40) {
+//     console.log("Your Grade is B");
+// } else if (marks >= 41 && marks <= 50) {
+//     console.log("Your Grade is A");
+// } else {
+//     console.log("Please enter number between 1 to 50");
+// }
+
+// let marks = Number(prompt("Enter Your Marks"))
+// let Grade;
+
+// switch (true) {
+//     case marks>=41 && marks <= 50: Grade = "A";
+//      break;
+//     case marks>= 31 && marks<= 40: Grade = "B";
+//     break;
+//     case marks>=21 && marks<= 30: Grade = "C";
+//     break;
+//     case marks>=11 && marks<= 20: Grade = "D";
+//     break;
+//     default:Grade = "E"  
+
+// }
+// console.log(Grade);
+
+// // Q2. Get Value
+
+// let char = prompt("Enter char")
+// if(char== P || char== p){
+//     console.log("PrepBytes");
+// } else if(char==Z || char==z){
+//     console.log("Zenith");
+// } else if(char== E || char == e){
+//     console.log("Expert Coder");
+// } else if( char==D || char==d) {
+//     console.log("Data Structure");
+// }
+
+//Q3. Find the maximum out of three numbers.
+
+// let num = [2, 5, 4]
+
+// console.log(Math.max(...num));
+
+//Q4. Find the second smallest number.
+
+
+// let num1 = Number(prompt("Enter your 1sr number"));
+// let num2 = Number(prompt("Enter your 2sr number"));
+// let num3 = Number(prompt("Enter your 3sr number"));
+
+// if((num1<= num2 && num2<= num3) ||(num3<= num2 && num2<=num1)){
+//     console.log(num2);
+// } else if((num2<=num1 && num1<=num3) || (num3<= num1 && num1<=num2)){
+//     console.log(num1);
+// } else {
+//     console.log(num3);
+// }
+
+// Q5. Check whether the triangle is Acute or Obtuse  ?
+
+// let angle1 = Number(prompt("Enter angle 1"));
+// let angle2 = Number(prompt("Enter angle 2"));
+// let angle3 = Number(prompt("Enter angle 3"));
+
+// if (angle1 + angle2 + angle3 === 180){
+    // if(angle1<90 && angle2<90 && angle3<90 ){
+    //     console.log("Triangle is Acute");
+    // } else if(angle1 > 90 || angle2 > 90 || angle3 > 90){
+    //     console.log("Triangle is Obtuse");
+    // }
